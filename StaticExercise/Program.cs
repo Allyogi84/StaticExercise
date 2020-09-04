67a5cbae171cd5fbe453520d724717e09c8bfb0d
Anthony Allgood <62567954+Allyogi84@users.noreply.github.com>
2020-09-04 11:51:37 -0400

    new code added

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var celcius = TempConverter.FahrenheitToCelsius(68); //20
+            var fahrenheit = TempConverter.CelciusToFahrenheit(20); //68
+
+            //Console = Class, WriteLine = method in a class, which is a static method
+            Console.WriteLine("After conversion:");
+            Console.WriteLine($"celcius {celcius}");
+            Console.WriteLine($"fahrenheit {fahrenheit}");
+
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        private static int celsius;
+
+        public static double FahrenheitToCelsius(double fahrenheit)
+
+        {
+            var result = (fahrenheit - 32) / 1.8;
+
+                 return result;
+            
+
+                
+        }
+        public static double CelciusToFahrenheit(double celcius)
+
+        {
+            return celsius * 9 / 5 + 32;
+
+        }
+    }
+
+}
+
